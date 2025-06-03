b17d6255e53a7de0c9572f7ca5bf801a28f08987
Davi <fernandesvieiradavi@gmail.com>
2025-06-03 13:37:58 -0300

    chore: Initial project structure with backend and frontend folders

---
+++ backend/src/main/java/br/com/progress/progress_api/ProgressApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.progress.progress_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressApiApplication.class, args);
+	}
+
+}
